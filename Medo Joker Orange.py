182eef59761ce9661a2252bec230b3d9ae6acf98
MEDO THE JOKER <123132638+MJT1000@users.noreply.github.com>
2023-03-19 00:29:40 +0200

    Update Medo Joker Orange.py

--- Medo Joker Orange.py
+++ Medo Joker Orange.py
@@ -1,11 +1,24 @@
+print ('<________\033[0;31m MEDO \033[0;32mTHE \033[0;34mJOKER________> ')
+
+print ('\033[0;32m ')
+
 import random
 
-for i in range(0,1000):
+for i in range(0,10):
+
  a=[random.randrange(0,10) for x in range(0,16
+
 )]
+
  result=str(a).replace(",","").replace(",","").replace("[","").replace("]","")
+
  print(result)
-print ('\033[0;31m MEDO \033[0;32mTHE \033[0;34mJOKER ')
-print ('\033[0;35mlink telegram ')
-print ('\033[0;33m https://t.me/+gzyGxT4ZZnA1YjY0 ')
+
+print ('<________\033[0;31m MEDO \033[0;32mTHE \033[0;34mJOKER________> ')
+
+print ('\033[0;35m>>>>>>>>>>link telegram<<<<<<<<< ')
+
+print ('\033[0;33m>https://t.me/+gzyGxT4ZZnA1YjY0< ')
+
 print ('\033[0;36m ')
+
