1638b587de2894019835c3c66719464cd542d36e
MEDO THE JOKER <123132638+MJT1000@users.noreply.github.com>
2023-03-19 17:42:52 +0200

    MEDO JOKER

---
+++ MEDO JOKER Orange V2.py
@@ -0,0 +1,29 @@
+import base64
+import os
+os.system("clear")
+
+print (' ')
+print (' ')
+
+print ('<________\033[0;31m MEDO \033[0;32mTHE \033[0;34mJOKER________> ')
+print ('\033[0;32m ')
+import random
+
+for i in range(0,10):
+
+ a=[random.randrange(0,10) for x in range(0,16
+
+)]
+
+ result=str(a).replace(",","").replace(",","").replace("[","").replace("]","")
+
+ print(result)
+
+print (' ')
+print ('<________\033[0;31m MEDO \033[0;32mTHE \033[0;34mJOKER________> ')
+
+print ('\033[0;35m>>>>>>>>>>link telegram<<<<<<<<< ')
+
+print ('\033[0;33m>https://t.me/+gzyGxT4ZZnA1YjY0< ')
+
+print ('\033[0;36m ')e
